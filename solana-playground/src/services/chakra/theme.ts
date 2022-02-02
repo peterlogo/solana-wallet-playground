1c6867be8854b9fd972edcb0baa98a3b4622910a
Peter Logo <peterlogo77@gmail.com>
2022-02-02 15:30:53 -0500

    Update

---
+++ solana-playground/src/services/chakra/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    primary: '#24243E',
+    shade: '#32324C'
+  },
+  fonts: {
+    heading: 'Open Sans',
+    body: 'Open Sans'
+  }
+});
